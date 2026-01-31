d0144ab3f0f2b01fd3671ca44a4b72a7735e42c7
Yakuwa Fred <yakuwafredrick@gmail.com>
2026-01-31 03:01:25 +0300

    Clean server.js: remove duplicate declarations and WebSocket code

--- server.js
+++ server.js
@@ -1,35 +1,20 @@
-const PORT = process.env.PORT || 3000;
-
-const server = app.listen(PORT, () => {
-  console.log('Chat server running on port ' + PORT);
-});
-
-const express = require('express');
-const WebSocket = require('ws');
-
-const app = express();
-app.use(express.static('public'));
-
-const server = app.listen(3000, () => {
-  console.log('Chat server running on port 3000');
-});
-
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', ws => {
+// server.js
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const path = require("path");
 
+// Create Express app
 const app = express();
 const server = http.createServer(app);
+
+// Create Socket.IO server
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
-
+// Use static folder "public"
 app.use(express.static(path.join(__dirname, "public")));
 
+// Socket.IO events
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
@@ -42,40 +27,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
+// Start server
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, "public")));
-
-io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
-
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("A user disconnected:", socket.id);
-  });
-});
-
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});  ws.on('message', message => {
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message.toString());
-      }
-    });
-  });
 });
